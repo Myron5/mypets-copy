bf974d0467727b2a99dca38fd3a473f1bfe387ae
ivan1melnyk <melnyk.ivan20a@gmail.com>
2024-06-14 14:31:36 +0200

    fix get news 2

--- frontend/src/service/api/apiNews.js
+++ frontend/src/service/api/apiNews.js
@@ -1,6 +1,8 @@
 import { instance } from './api';
 
 export const fetchNews = async (searchNews, page, perPage) => {
-  const data = await instance.get('/notices/news');
+  const data = await instance.get(
+    `/notices/news?searchNews=${searchNews}&page=${page}&perPage=${perPage}`
+  );
   return data;
 };
